30081e53be7038bbd994c3877084a893c16ecbba
anthony2v <anthony_2v@hotmail.com>
2021-02-25 21:05:02 -0500

    Implemented parse_message function.

--- irc_core/message_handler.py
+++ irc_core/message_handler.py
@@ -2,13 +2,40 @@ class MessageHandler:
     def __init__(self):
         self.cmds = {}
         self.validators = {}
+    
     def command(self, cmd):
         def _decorator(func):
             self.cmds[cmd] = func
             return func
         return _decorator
+    
     def validator(self, cmd):
         def _decorator(func):
             self.validators[cmd] = func
             return func
         return _decorator
+    
+    def handle_message(self, connection, message):
+        self._parse_message(message)
+        pass
+
+    def _parse_message(self, message):
+        message = message.replace(b"\r\n", b"")
+
+        prefix = None
+        if message.startswith(b':'):
+            first_space = message.index(b' ')
+            prefix = message[1:first_space]
+            message = message[first_space+1:].lstrip(b' ')
+
+        trailing_start = message.find(b':')
+        trailing = []
+        if trailing_start != -1:   
+            trailing.append(message[trailing_start+1:])
+            message = message[:trailing_start]
+
+        cmd, *params = message.split(b' ')
+        params = list(p for p in params if p)
+        
+        params += trailing
+        return cmd, prefix, params
--- tests/test_message_handler.py
+++ tests/test_message_handler.py
@@ -1,3 +1,4 @@
+from server.server import Connection
 from unittest import mock
 from irc_core.message_handler import MessageHandler
 
@@ -7,10 +8,10 @@ def test_message_handlers_can_be_bound():
     mock_fn = mock.MagicMock()
 
     @ch.command('NICK')
-    def fn():
+    def fn(send, *params, prefix = None):
         mock_fn()
 
-    ch.cmds['NICK']()
+    ch.cmds['NICK'](mock.MagicMock())
     mock_fn.assert_called()
 
 def test_validators_can_be_bound():
@@ -19,8 +20,50 @@ def test_validators_can_be_bound():
     mock_fn = mock.MagicMock()
 
     @ch.validator('NICK')
-    def fn():
+    def fn(*params, prefix = None):
         mock_fn()
 
     ch.validators['NICK']()
     mock_fn.assert_called()
+
+def test_handle_message_calls_parser():
+    ch = MessageHandler()
+
+    mock_connection = mock.MagicMock(spec = Connection)
+    msg = b"NICK\r\n"
+
+    ch._parse_message = mock.MagicMock()
+
+    ch.handle_message(
+        connection = mock_connection,
+        message = msg
+    )
+
+    ch._parse_message.assert_called_with(msg)
+
+def test_parse_message_returns_command_name():
+    ch = MessageHandler()
+
+    msg = b":WiZ NICK Kilroy\r\n"
+
+    command, prefix, params = ch._parse_message(msg)
+
+    assert command == b"NICK"
+    assert prefix == b"WiZ"
+    assert params == [b"Kilroy"]
+
+    msg = b"NICK Wiz\r\n"
+
+    command, prefix, params = ch._parse_message(msg)
+
+    assert command == b"NICK"
+    assert prefix == None
+    assert params == [b"Wiz"]
+
+    msg = b":Angel PRIVMSG Wiz :Hello are you receiving this message ?\r\n"
+
+    command, prefix, params = ch._parse_message(msg)
+    print(params)
+    assert command == b"PRIVMSG"
+    assert prefix == b"Angel"
+    assert params == [b"Wiz", b"Hello are you receiving this message ?"]
